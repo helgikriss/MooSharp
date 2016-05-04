e087a5327e1bed0b6735754414c72c9dd492f7b7
Smari <smarig15@ru.is>
2016-05-04 15:43:35 +0000

    Gunni og Helgi voru memm. Assignment service

--- MooSharpSolution/MooSharp/Services/AssignmentsService.cs
+++ MooSharpSolution/MooSharp/Services/AssignmentsService.cs
@@ -15,28 +15,28 @@ namespace MooSharp.Services
 			_db = new ApplicationDbContext();
 		}
 
-        public list<AssignmentViewModel> GetAssignmentsByCourseId(int courseID) {
+        public List<AssignmentViewModel> GetAssignmentsByCourseId(int courseID) {
 			// TODO: Finish GetAssignmentsByCourseId() method.
 			return null;
         }
 
-		public AssignmentViewModel GetAssignmentById(int assignmentID) {
-			var assignment = _db._assignments.SingleOrDefault(x => x._ID == assignmentID);
-			if(assignment == null) {
-				// TODO: Throw exception.
-			}
+        public AssignmentViewModel GetAssignmentById(int assignmentID) {
+            var assignment = _db._assignments.SingleOrDefault(x => x._ID == assignmentID);
+            if (assignment == null) {
+                // TODO: Throw exception.
+            }
 
-			var milestones = _db._milestones
-				.Where(x => x._assignmentID == assignmentID)
-				.Select(x => new MilestoneViewModel {
-					_title = x._title;
-				})
-				.ToList();
+            var milestones = _db._milestones
+                .Where(x => x._assignmentID == assignmentID)
+                .Select(x => new MilestoneViewModel {
+                    _title = x._title
+                })
+                .ToList();
 
-			var viewModel = new AssignmentViewModel {
-				_title = assignment._title;
-				_milestones = milestones;
-			};
+            var viewModel = new AssignmentViewModel {
+                _title = assignment._title,
+                _milestones = milestones
+            };
 
 			return viewModel;
 		}
