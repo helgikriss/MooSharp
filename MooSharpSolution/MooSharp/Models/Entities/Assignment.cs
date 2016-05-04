e66ba1d42ce101450c2e74786a67a518c23e5b1b
hlynurstef <hlynurs15@ru.is>
2016-05-04 13:15:10 +0000

    Add VisualStudio project to repo

---
+++ MooSharpSolution/MooSharp/Models/Entities/Assignment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MooSharp.Models.Entities
+{
+    public class Assignment
+    {
+		public int _ID { get; set; }
+		public int _courseID { get; set; }
+		public string _title { get; set; }
+		public string _description { get; set; }
+    }
+}
---
+++ MooSharpSolution/MooSharp/Services/AssignmentsService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MooSharp.Models.ViewModels;
+
+namespace MooSharp.Services
+{
+    public class AssignmentsService
+    {
+        public list<AssignmentViewModel> GetAssignmentsByCourseId(int courseID) {
+            // TODO: 
+            return null;
+        }
+    }
+}
