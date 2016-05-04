350b9d62396d0ee134a11cf3fb2ba46e89dc646b
hlynurstef <hlynurs15@ru.is>
2016-05-04 23:16:59 +0000

    Add properties and comments to Submission and Course, update project file

--- MooSharpSolution/MooSharp/Models/Entities/Course.cs
+++ MooSharpSolution/MooSharp/Models/Entities/Course.cs
@@ -5,7 +5,28 @@ using System.Web;
 
 namespace MooSharp.Models.Entities
 {
+	/// <summary>
+	/// A Course represents a single academic course.
+	/// A Course can have multiple assignments and multiple users 
+	/// associated with it.
+	/// </summary>
     public class Course
     {
-    }
+		/// <summary>
+		/// The database-generated unique ID of the course.
+		/// </summary>
+		public int _ID { get; set; }
+
+		/// <summary>
+		/// Contains the course number which can be both alpha and numerical characters.
+		/// Example: "T-220-VLN2".
+		/// </summary>
+		public string _courseNumber { get; set; }
+
+		/// <summary>
+		/// Contains the title of the course.
+		/// Example: "Verklegt námskeið 2"
+		/// </summary>
+		public string _title { get; set; }
+	}
 }
--- MooSharpSolution/MooSharp/Models/Entities/Submission.cs
+++ MooSharpSolution/MooSharp/Models/Entities/Submission.cs
@@ -5,7 +5,46 @@ using System.Web;
 
 namespace MooSharp.Models.Entities
 {
+	/// <summary>
+	/// A Submission represents a single submission to a milestone.
+	/// Submission contains the date and time of submission, where the
+	/// submitted file is located on the server, the status of the submission
+	/// and a list of strings with the obtained output.
+	/// </summary>
 	public class Submission
 	{
+		/// <summary>
+		/// The database-generated unique ID of the submission.
+		/// </summary>
+		public int _ID { get; set; }
+
+		/// <summary>
+		/// The foreign key to the milestone that this submission is a part of.
+		/// </summary>
+		public int _milestoneID { get; set; }
+
+		/// <summary>
+		/// Contains the date and time of submission.
+		/// Example: 1/1/1970 00:00:00
+		/// </summary>
+		public DateTime _submissionDateTime { get; set; }
+
+		/// <summary>
+		/// Contains the path to the submitted file.
+		/// Example: "~\submissions\user\hello.cpp"
+		/// </summary>
+		public string _submissionPath { get; set; }
+
+		/// <summary>
+		/// Contains the status of the submission after it has been compiled, run and checked.
+		/// status can contain for example "Accepted", "Wrong Answer", "Memory Error", etc.
+		/// </summary>
+		public string _status { get; set; }
+
+		/// <summary>
+		/// Contains a list of all the outputs that were obtained from running some input on it.
+		/// Each item in the list is an output from a single time the program was run.
+		/// </summary>
+		public List<string> _outputs { get; set; }
 	}
 }
