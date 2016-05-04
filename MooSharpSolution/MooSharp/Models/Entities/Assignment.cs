fd61c019f7654c533a27786dc16036c68feb0198
hlynurstef <hlynurs15@ru.is>
2016-05-04 14:59:26 +0000

    Add a few Entity/Service/Controller classes

--- MooSharpSolution/MooSharp/Models/Entities/Assignment.cs
+++ MooSharpSolution/MooSharp/Models/Entities/Assignment.cs
@@ -1,15 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace MooSharp.Models.Entities
+﻿namespace MooSharp.Models.Entities
 {
+	/// <summary>
+	/// An Assignment represents a single assignment in a course.
+	/// </summary>
     public class Assignment
     {
+		/// <summary>
+		/// The database-generated unique ID of the assignment.
+		/// </summary>
 		public int _ID { get; set; }
+
+		/// <summary>
+		/// The foreign key to the course that this assignment is a part of.
+		/// </summary>
 		public int _courseID { get; set; }
+
+		/// <summary>
+		/// Contains the name of the assignment.
+		/// Example: "Lab 1"
+		/// </summary>
 		public string _title { get; set; }
+
+		/// <summary>
+		/// Contains the description for the assignment.
+		/// This description is the value 
+		/// Example: "This assignment contains two milestones, each weighing 50% of the grade of the assignment".
+		/// </summary>
 		public string _description { get; set; }
     }
 }
---
+++ MooSharpSolution/MooSharp/Models/Entities/Milestone.cs
@@ -0,0 +1,35 @@
+﻿namespace MooSharp.Models.Entities
+{
+	/// <summary>
+	/// A Milestone represents a single part of an assignment.
+	/// Each assignment can contain one or more milestones and each milestone
+	/// weighs a certain percentage of the final grade of the assignment.
+	/// </summary>
+	public class Milestone
+	{
+		/// <summary>
+		/// The database-generated unique ID of the milestone.
+		/// </summary>
+		public int _ID { get; set; }
+
+		/// <summary>
+		/// The foreign key to the assignment that this milestone is a part of.
+		/// </summary>
+		public int _assignmentID { get; set; }
+
+		/// <summary>
+		/// Contains the name of the milestone.
+		/// Example: "Part 1"
+		/// </summary>
+		public string _title { get; set; }
+
+		/// <summary>
+		/// Determines how much this milestone weighs of the entire assignment.
+		/// Example: If the milestone is 15% of the grade of the assignment then
+		/// this property contains the value 15.
+		/// </summary>
+		public int _weight { get; set; }
+
+		// TODO: Add input/output property.
+	}
+}
