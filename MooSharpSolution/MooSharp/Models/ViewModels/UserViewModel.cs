5ad68affe4c5dd762d36670175c7d7b86fc81d4a
Smari <smarig15@ru.is>
2016-05-07 22:06:56 +0000

    admin index list users and users from database

---
+++ MooSharpSolution/MooSharp/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MooSharp.Models.ViewModels
+{
+	public class UserViewModel
+	{
+		public string userId { get; set; }
+ 		public string username { get; set; }
+ 		public string email { get; set; }
+ 		public string role { get; set; }
+	}
+}
