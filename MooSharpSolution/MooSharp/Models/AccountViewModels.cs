acfc479b87251b4657049280312b547a2acded04
hlynurstef <hlynurs15@ru.is>
2016-05-07 16:27:59 +0000

    minor login screen adjustments

--- MooSharpSolution/MooSharp/Models/AccountViewModels.cs
+++ MooSharpSolution/MooSharp/Models/AccountViewModels.cs
@@ -49,7 +49,7 @@ namespace MooSharp.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
+        [Display(Name = "Username")]
         //[EmailAddress]
         public string Email { get; set; }
 
