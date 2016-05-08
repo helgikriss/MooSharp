b3620c88d3207183d44533d7acafca51f3f4e459
Smari <smarig15@ru.is>
2016-05-08 23:50:00 +0000

    teacher folder búið til og CreateMilestoneViewmodel líka

---
+++ MooSharpSolution/MooSharp/Models/ViewModels/Teachers/CreateMilestoneViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MooSharp.Models.ViewModels.Teachers
+{
+	public class CreateMilestoneViewModel
+	{
+		[Required]
+		[Display(Name = "AssignmentID")]
+		public int AssignmentID { get; set; }
+
+		[Required]
+		[Display(Name = "Title")]
+		public string Title { get; set; }
+
+		[Required]
+		[Display(Name = "Weight")]
+		public int Weight { get; set; }
+	}
+}
