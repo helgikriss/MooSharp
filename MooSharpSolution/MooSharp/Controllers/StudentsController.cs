b51df045f5e658f91153cfa475be8ebda1b334f0
hlynurstef <hlynurs15@ru.is>
2016-05-08 14:19:51 +0000

    set modal window on login to 40% top margin

--- MooSharpSolution/MooSharp/Controllers/StudentsController.cs
+++ MooSharpSolution/MooSharp/Controllers/StudentsController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MooSharp.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace MooSharp.Controllers
 {
