cb2e770524457c2de8a55af0242c8895a78c8ad6
hlynurstef <hlynurs15@ru.is>
2016-05-10 16:06:58 +0000

    Add ConnectUserToCourse view

--- MooSharpSolution/MooSharp/Models/ViewModels/Admins/ConnectUserToCourseViewModel.cs
+++ MooSharpSolution/MooSharp/Models/ViewModels/Admins/ConnectUserToCourseViewModel.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using MooSharp.Models.Entities;
 
 namespace MooSharp.Models.ViewModels.Admins {
 	public class ConnectUserToCourseViewModel {
+		[Required]
+		[Display(Name = "CourseID")]
 		public int CourseID { get; set; }
 
+		[Required]
+		[Display(Name = "UserID")]
 		public string UserID { get; set; }
 
+		[Required]
+		[Display(Name = "Role")]
 		public string Role { get; set; }
+
+		public List<CourseViewModel> AllCourses { get; set; }
+
+		public List<UserViewModel> AllUsers { get; set; }
 	}
 }
