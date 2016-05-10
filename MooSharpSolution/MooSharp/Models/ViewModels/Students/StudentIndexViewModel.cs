4e3a78d3861500516ad72e2a221aecf841fdadcb
Smari <smarig15@ru.is>
2016-05-10 21:53:26 +0000

    studentcontroller og index

---
+++ MooSharpSolution/MooSharp/Models/ViewModels/Students/StudentIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MooSharp.Models.ViewModels.Students
+{
+	public class StudentIndexViewModel
+	{
+		public List<AssignmentViewModel> ActiveAssignments { get; set; }
+
+		public List<AssignmentViewModel> FinishedAssignments { get; set; }
+
+		public List<CourseViewModel> MyCourses { get; set; }
+	}
+}
